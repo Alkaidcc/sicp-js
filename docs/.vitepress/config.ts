66b600804474c0effade5e4d687c6932df8f9fd9
Alkaidcc <alkaidcc@qq.com>
2023-04-07 16:51:11 +0800

    docs: figure and footnote support

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitepress'
 import {en} from './locales/en'
 import {zhCN} from './locales/zh'
 import mathjax3 from 'markdown-it-mathjax3';
+import MarkdownItFootnote from 'markdown-it-footnote';
+import implicitFigures from 'markdown-it-image-figures';
 
 const customElements = ['mjx-container'];
 export default defineConfig({
@@ -12,7 +14,7 @@ export default defineConfig({
   },
   markdown: {
     config: (md) => {
-      md.use(mathjax3)
+      md.use(mathjax3).use(MarkdownItFootnote).use(implicitFigures,{figcaption: true});
     }
   },
   vue: {
